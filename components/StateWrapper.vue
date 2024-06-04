fcbc5651cfc56262c82ba185206783c8410c337b
JoshCunningHum <jkdimpas@gmail.com>
2024-06-04 13:59:46 +0800

    Create StateWrapper

---
+++ components/StateWrapper.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const over = ref(false);
+</script>
+
+<template>
+    <div
+        @mouseover="over = true"
+        @mouseout="over = false"
+    >
+        <slot :over="over"></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
